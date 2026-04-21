cfb72f62de02b067344bde7f5f44d88845423f60
unknown <vinod.h@simplify.tech>
2026-04-21 12:44:25 +0530

    second commit - JobTracker backend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/jobs", require("./routes/jobroutes"));
+app.use("/api/jobs", require("./routes/jobRoutes"));
 
 app.get('/', (req, res) => {
     res.send('API is running...');
